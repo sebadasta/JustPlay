7da15789b5b6bba5e0dd2adf9a01c672662f2574
Jes <jes@MacBook-Air-de-Jes.local>
2017-01-13 14:42:26 +0000

    JustPlay

--- gmailLogInApp.js
+++ gmailLogInApp.js
@@ -7,3 +7,47 @@ function onSignIn(googleUser) {
   console.log('Email: ' + profile.getEmail());
 
 }
+
+
+$(document).ready(function() {
+
+  /* code here */
+  /**
+ * The Sign-In client object.
+ */
+var auth2;
+
+/**
+ * Initializes the Sign-In client.
+ */
+var initClient = function() {
+    gapi.load('auth2', function(){
+        /**
+         * Retrieve the singleton for the GoogleAuth library and set up the
+         * client.
+         */
+        auth2 = gapi.auth2.init({
+            client_id: '769395586214-jni7dp75c9nj18edh7louk47a6uj6bef.apps.googleusercontent.com'
+        });
+
+        // Attach the click handler to the sign-in button
+        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
+    });
+};
+
+/**
+ * Handle successful sign-ins.
+ */
+var onSuccess = function(user) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+};
+
+
+ });
