25a24d03ce95a24cd70a2e5a45355bedebd0c1ee
Jes <jes@MacBook-Air-de-Jes.local>
2017-01-13 14:16:19 +0000

    JustPlay

--- gmailLogInApp.js
+++ gmailLogInApp.js
@@ -1,8 +1,10 @@
 // gmail LogIn
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+   var id_token = googleUser.getAuthResponse().id_token;
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
+  console.console.log('token' + id_token);
 }
