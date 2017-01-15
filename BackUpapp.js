c884ad72203cd39e74a92c8272e79438a1cd6466
Jes <jes@MacBook-Air-de-Jes.local>
2017-01-15 21:23:01 +0000

    JustPlay

--- BackUpapp.js
+++ BackUpapp.js
@@ -1,15 +1,14 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('app', ['ngRoute', 'ngCookies'])
+    angular.module('app', ['ngRoute', 'ngCookies'])
         .config(config)
         .run(run);
 
     config.$inject = ['$routeProvider', '$locationProvider'];
     function config($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/', {
+            .when('/a', {
                 controller: 'HomeController',
                 templateUrl: 'home/home.view2.html',
                 controllerAs: 'vm'
