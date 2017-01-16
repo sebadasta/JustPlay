642d87497e2db568fc7707f18ebb3ded0fafb90b
Jes <jes@MacBook-Air-de-Jes.local>
2017-01-16 18:10:28 +0000

    JustPlay

--- restriccion/app.js
+++ restriccion/app.js
@@ -44,7 +44,7 @@
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register', '/nop']) === -1;
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
             var loggedIn = $rootScope.globals.currentUser;
             if (restrictedPage && !loggedIn) {
                 $location.path('/login');
